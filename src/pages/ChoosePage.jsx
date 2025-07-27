69eff577c4c9fbf4e4e32c694ef59fa0a4651ddb
HaseethVardhan <haseethvardhan01@gmail.com>
2025-07-27 16:51:16 +0530

    added backgorund.

--- src/pages/ChoosePage.jsx
+++ src/pages/ChoosePage.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const ChoosePage = () => {
   return (
-    <div>Choose</div>
+    <div className="min-h-screen w-full relative bg-black">
+    <div
+      className="absolute inset-0 z-0"
+      style={{
+        background: "radial-gradient(ellipse 80% 60% at 50% 0%, rgba(226, 232, 240, 0.15), transparent 70%), #000000",
+      }}
+    />
+  
+    
+  </div>
   )
 }
 
