9fac0a6d7ab031117cf8d6b97caaff7b6376690f
HaseethVardhan <haseethvardhan01@gmail.com>
2025-12-31 12:23:37 +0530

    fix: add vercel analytics component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Blogs from "./pages/Blogs";
 import BlogDetail from "./pages/BlogDetail";
-import { Analytics } from "@vercel/analytics/next"
 
 
 function App() {
